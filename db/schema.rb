0a03701f46f8974e7dedbf840b86f8cff5a102ab
David Keller <dckeller1@gmail.com>
2021-02-17 10:29:12 -0800

    Add closed bool to model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_17_005102) do
+ActiveRecord::Schema.define(version: 2021_02_17_182041) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2021_02_17_005102) do
     t.datetime "send_date"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "closed"
   end
 
 end
