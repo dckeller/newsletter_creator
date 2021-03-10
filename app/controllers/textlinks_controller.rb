58735882186c8409e9c3d2d140cfd2d708bb3e64
David Keller <dckeller1@gmail.com>
2021-03-10 14:50:16 -0700

    Cleaning up text links

--- app/controllers/textlinks_controller.rb
+++ app/controllers/textlinks_controller.rb
@@ -12,6 +12,23 @@ class TextlinksController < ApplicationController
 		redirect_to newsletter_path(@newsletter)
 	end
 
+	def edit
+		@newsletter = Newsletter.find(params[:newsletter_id])
+		@textlink = Textlink.find(params[:id])
+	end
+
+	def update
+    	@textlink = Textlink.find(params[:id])
+    	@textlink.update(textlink_params)
+    	redirect_to @newsletter 
+  	end
+
+  	def destroy
+  		@textlink = Textlink.find(params[:id])
+  		@textlink.destroy
+    	redirect_to @newsletter 
+  	end	
+
 	private
 
 	def textlink_params
